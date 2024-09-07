7c4a0b3ce6b3ac66daffe9c495f97a225f7fe626
Joao Meyer <1994meyer@gmail.com>
2024-09-07 12:51:14 -0300

    Added types dir

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -11,11 +11,11 @@ import type { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
 	interface Session extends DefaultSession {
-		// Example:
-		// user: {
-		//   name: string
-		//   avatar: string
-		//   role: 'admin' | 'manager' | 'user'
-		// }
+		user?: {
+			id: string;
+			name: string;
+			email: string;
+			image: string;
+		};
 	}
 }
