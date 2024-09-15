836b07a8cf45d465307a0d9af23cb71e59a3fbf6
Joao Meyer <1994meyer@gmail.com>
2024-09-15 14:19:10 -0300

    Trying to fix our DB in vercel

--- server/utils/db/client.ts
+++ server/utils/db/client.ts
@@ -1,10 +1,13 @@
+import dotenv from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 import { Checkouts, Creations, Images, Users } from "./schema";
 
-const config = useRuntimeConfig();
-const client = postgres(config.postgresUrl);
+dotenv.config();
+
+// @ts-ignore
+const client = postgres(process.env.POSTGRES_URL);
 
 export const db = drizzle(client, {
 	// logger: true,
