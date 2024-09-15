81dc02482600140d2ceea036dffb3ac4cf568dec
Joao Meyer <1994meyer@gmail.com>
2024-09-15 14:14:59 -0300

    Fixed DB

--- server/utils/db/client.ts
+++ server/utils/db/client.ts
@@ -4,11 +4,8 @@ import postgres from "postgres";
 import { Checkouts, Creations, Images, Users } from "./schema";
 
 const config = useRuntimeConfig();
-if (!config.public.POSTGRES_URL) {
-	throw new Error("POSTGRES_URL is not set");
-}
+const client = postgres(config.postgresUrl);
 
-const client = postgres(config.public.POSTGRES_URL);
 export const db = drizzle(client, {
 	// logger: true,
 	schema: { Users, Images, Creations, Checkouts },
