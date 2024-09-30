7c1f8e5d9a59c866c166d9426b28e83b95d6c8c2
gmbueno <gmbueno1802@gmail.com>
2024-09-30 17:05:44 -0300

    now removing credits, as should

--- server/api/inference.post.ts
+++ server/api/inference.post.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { useServerInferenceService } from "~/server/utils/services/inference";
+import { createUsersService } from "~/server/utils/services/users";
 
 const schema = z.object({
 	prompt: z.string().min(5).max(512),
@@ -10,9 +11,18 @@ export default defineEventHandler(async (event): Promise<CreationSelect> => {
 	const user = await assertAuthenticated(event);
 	const body = await readValidatedBody(event, (body) => schema.parse(body));
 
+	if (user.credits <= 0) {
+		console.info(`User ${user.id} has no credits: ${user.credits}`);
+		throw createError({ statusCode: 400, statusMessage: "not enough credits" });
+	}
+
 	return await db.transaction(async (tx) => {
+		const users = createUsersService(tx, event);
 		const service = useServerInferenceService(tx, event);
 		const image = await service.create(user, body.prompt, body.model);
+
+		user.credits -= 1;
+		await users.update(user);
 		return image;
 	});
 });
