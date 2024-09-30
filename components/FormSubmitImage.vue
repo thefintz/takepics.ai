e8ccb3647e22574d21d6067bd44748705574c6e3
gmbueno <gmbueno1802@gmail.com>
2024-09-30 17:05:56 -0300

    adding back prompts

--- components/FormSubmitImage.vue
+++ components/FormSubmitImage.vue
@@ -1,7 +1,29 @@
 <template>
   <form @submit.prevent="() => execute()">
-		<InputText v-model="prompt" placeholder="Enter a prompt" />
-		<Select v-model="model" :options="models ?? []" optionLabel="id" optionValue="id" placeholder="Select Model" />
+    <!-- First Dropdown: Specify optionValue=name -->
+    <Dropdown
+      class="min-w-60 max-w-full"
+      v-model="selectedCategory"
+      :options="categories"
+      optionLabel=name
+      optionValue=name
+      placeholder="Select category"
+      @change="onCategoryChange"
+    />
+    
+    <!-- Second Dropdown: Remains mostly unchanged -->
+    <Dropdown
+      class="min-w-60 max-w-full ml-4"
+      v-model="selectedPrompt"
+      :options="filteredPrompts"
+      optionLabel=name
+      optionValue=prompt
+      placeholder="Select style"
+      :disabled="!selectedCategory"
+    />
+
+		<!-- Model (name) selector -->
+		<Select v-model="model" :options="models ?? []" optionLabel="id" optionValue="id" placeholder="Select Model" class="min-w-60 max-w-full ml-4" />
     <Button class="ml-4" label="Create Images" type="submit"></Button>
   </form>
 </template>
@@ -9,6 +31,31 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 import type { CreationSelect } from "~/server/utils/db/schema";
+
+// State Variables
+const selectedCategory = ref<string | null>(null);
+const selectedPrompt = ref<string>("");
+
+import promptOptions from '~/assets/promptOptions.json';
+
+// Compute unique categories
+const categories = computed(() => {
+  const uniqueCategories = [...new Set(promptOptions.map(option => option.category))];
+  return uniqueCategories.map(category => ({ name: category }));
+});
+
+// Update filteredPrompts based on selectedCategory
+const filteredPrompts = computed(() => {
+  if (selectedCategory.value) {
+    return promptOptions.filter(option => option.category === selectedCategory.value);
+  }
+  return [];
+});
+
+// Handle category change
+const onCategoryChange = () => {
+  selectedPrompt.value = "";
+};
 
 // Define Emits
 const emits = defineEmits<(e: "response", data: CreationSelect) => void>();
