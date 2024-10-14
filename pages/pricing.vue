c5f9ef2bf2c7ab5b4f67a76ae447eed7b213ae99
Maurício <mauriciomb97@gmail.com>
2024-10-14 10:51:40 -0300

    replacing | by _ on user when sending to stripe

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -6,7 +6,7 @@
         :pricing-table-id="'prctbl_1Q84prP2aEWXlznlThXwZ00r'"
         :publishable-key="'pk_test_51PxqQFP2aEWXlznl5pMNEs2KFFRxQ4k47rGyUjmmdRJpNYIMqdURdAXlnEOdOhXBL9xoiGKiFbCMEXCKOkedfN5v00FdpFFUiL'"
         v-if="session && session.user"
-        :client-reference-id="session.user.id"
+        :client-reference-id="session.user.id.replace(/\|/g, '_')"
         :customer-email="session.user.email"
       />
     </div>
@@ -15,6 +15,4 @@
 
 <script lang="ts" setup>
 const { data: session } = useAuth();
-
-
 </script>
