f1b1822073c2645a4db82406715ebd12ac383098
Maurício <mauriciomb97@gmail.com>
2024-10-24 11:31:28 -0300

    adding link to subscription management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,122 +1,122 @@
 import Aura from "@primevue/themes/aura";
 
 export default defineNuxtConfig({
-				compatibilityDate: "2024-08-09",
+    compatibilityDate: "2024-08-09",
 
-				typescript: {
-								typeCheck: true,
-				},
+    typescript: {
+        typeCheck: true,
+    },
 
-				devtools: {
-								enabled: true,
-				},
+    devtools: {
+        enabled: true,
+    },
 
-				scripts: {
-								registry: {
-										stripe: true,
-								}
-				},
-				modules: [
-				 "@nuxt/image",
-				 "@nuxtjs/tailwindcss",
-				 "@primevue/nuxt-module",
-				 "@sidebase/nuxt-auth",
-				 "nuxt-posthog",
-				 "@vueuse/nuxt",
-				 "@nuxt/scripts",
-				],
+    scripts: {
+        registry: {
+            stripe: true,
+        }
+    },
+    modules: [
+        "@nuxt/image",
+        "@nuxtjs/tailwindcss",
+        "@primevue/nuxt-module",
+        "@sidebase/nuxt-auth",
+        "nuxt-posthog",
+        "@vueuse/nuxt",
+        "@nuxt/scripts",
+    ],
 
-				// https://tailwind.primevue.org/nuxt/
-				// https://github.com/primefaces/primevue-examples/blob/main/nuxt-styled-tailwind/nuxt.config.ts
-				primevue: {
-								options: {
-												theme: {
-																preset: Aura,
-												},
-								},
-				},
+    // https://tailwind.primevue.org/nuxt/
+    // https://github.com/primefaces/primevue-examples/blob/main/nuxt-styled-tailwind/nuxt.config.ts
+    primevue: {
+        options: {
+            theme: {
+                preset: Aura,
+            },
+        },
+    },
 
-				// Files added here are included in all pages. We want to add the PrimeIcons
-				// CSS
-				//
-				// https://nuxt.com/docs/api/nuxt-config#css
-				// https://github.com/primefaces/primeicons
-				// https://primevue.org/icons/
-				css: ["primeicons/primeicons.css"],
+    // Files added here are included in all pages. We want to add the PrimeIcons
+    // CSS
+    //
+    // https://nuxt.com/docs/api/nuxt-config#css
+    // https://github.com/primefaces/primeicons
+    // https://primevue.org/icons/
+    css: ["primeicons/primeicons.css"],
 
-				auth: {
-								// This makes all pages private by default. If you want to make a page
-								// public, you can use `definePageMeta({ auth: false })` on the page `setup`
-								//
-								// https://auth.sidebase.io/guide/application-side/configuration#globalappmiddleware
-								globalAppMiddleware: true,
+    auth: {
+        // This makes all pages private by default. If you want to make a page
+        // public, you can use `definePageMeta({ auth: false })` on the page `setup`
+        //
+        // https://auth.sidebase.io/guide/application-side/configuration#globalappmiddleware
+        globalAppMiddleware: true,
 
-								// We can change this depending on the provider we deploy to. Most have
-								// an environment variable that contains the URL
-								// originEnvKey: "AUTH_ORIGIN",
+        // We can change this depending on the provider we deploy to. Most have
+        // an environment variable that contains the URL
+        // originEnvKey: "AUTH_ORIGIN",
 
-								// https://auth.sidebase.io/guide/authjs/quick-start#configuration
-								provider: {
-												type: "authjs",
-												trustHost: false,
-												defaultProvider: "auth0",
-												addDefaultCallbackUrl: "/",
-								},
-				},
+        // https://auth.sidebase.io/guide/authjs/quick-start#configuration
+        provider: {
+            type: "authjs",
+            trustHost: false,
+            defaultProvider: "auth0",
+            addDefaultCallbackUrl: "/",
+        },
+    },
 
-				// Configurations from here should be overriden by using `.env` or environment
-				// variables. Check `.env.example` for documentation on each of those values
-				//
-				// https://nuxt.com/docs/guide/going-further/runtime-config
-				runtimeConfig: {
-								secret: "",
+    // Configurations from here should be overriden by using `.env` or environment
+    // variables. Check `.env.example` for documentation on each of those values
+    //
+    // https://nuxt.com/docs/guide/going-further/runtime-config
+    runtimeConfig: {
+        secret: "",
 
-								postgresUrl: "",
+        postgresUrl: "",
 
-								supabase: {
-												url: "",
-												key: "",
-												bucket: ""
-								},
+        supabase: {
+            url: "",
+            key: "",
+            bucket: ""
+        },
 
-								auth0: {
-												issuer: "",
-												clientId: "",
-												clientSecret: "",
-								},
+        auth0: {
+            issuer: "",
+            clientId: "",
+            clientSecret: "",
+        },
 
-								replicate: {
-												username: "",
-												apiToken: "",
-												model: "",
-												version: "",
-												webhookSecret: "",
-												webhookUrl: "",
-												webhookUrlTrainings: "",
-								},
+        replicate: {
+            username: "",
+            apiToken: "",
+            model: "",
+            version: "",
+            webhookSecret: "",
+            webhookUrl: "",
+            webhookUrlTrainings: "",
+        },
 
-								stripe: {
-												priceId: "",
-												imageCreditsPerCheckout: 100,
-												trainingCreditsPerCheckout: 1,
-												secretKey: "",
-												webhookSecret: "",
-												successUrl: "",
-								},
+        stripe: {
+            priceId: "",
+            imageCreditsPerCheckout: 100,
+            trainingCreditsPerCheckout: 1,
+            secretKey: "",
+            webhookSecret: "",
+            successUrl: "",
+        },
 
-								// Configurations inside the `public` object are available to the client.
-								// Eg. in the browser
-								public: {
-												posthog: {
-																// Uncomment this to disable PostHog
-																disabled: true,
-												},
-								},
-				},
+        // Configurations inside the `public` object are available to the client.
+        // Eg. in the browser
+        public: {
+            posthog: {
+                // Uncomment this to disable PostHog
+                disabled: true,
+            },
+        },
+    },
 
-				nitro: {
-								experimental: {
-												openAPI: true,
-								},
-				},
+    nitro: {
+        experimental: {
+            openAPI: true,
+        },
+    },
 });
--- pages/profile.vue
+++ pages/profile.vue
@@ -3,9 +3,9 @@
     <!-- Header: Profile Picture -->
     <template #header>
       <div class="flex items-center gap-4 p-8">
-        <Avatar size="xlarge">
-          <NuxtImg alt="Profile Picture" :src="data?.image ?? placeholderImage" />
-        </Avatar>
+<!--        <Avatar size="xlarge">-->
+<!--          <NuxtImg alt="Profile Picture" :src="data?.image ?? placeholderImage" />-->
+<!--        </Avatar>-->
         <div>
           <h2 class="text-xl font-semibold">{{ data?.email }}</h2>
           <p v-if="data?.createdAt" class="text-gray-400">Created: {{ formatDate(data.createdAt) }}</p>
@@ -20,8 +20,10 @@
           <h4 class="mb-2 text-xl font-semibold text-gray-100">Subscription</h4>
           <p v-if="data?.name" class="text-gray-400"><strong>Training credits:</strong> {{ data?.trainingCredits }}</p>
           <p v-if="data?.name" class="text-gray-400"><strong>Image credits:</strong> {{ data?.imageCredits }}</p>
-          <p class="text-gray-400"><strong> Current plan: (hardcoded) PRO</strong></p>
-          <p class="text-gray-400"><strong> Next renewal: (hardcoded) November, 2, 2024</strong></p>
+          <p class="text-gray-400">
+            <a href="https://billing.stripe.com/p/login/test_cN2dS8c210D6aDC144" class="cursor-pointer text-blue-500 underline">
+              View Current Plan</a>
+          </p>
         </div>
 
         <!-- Additional Information for Development Purpose -->
