b2ea373b86d5189dc5410cb29b14ef0cd8297dd0
Maurício <mauriciomb97@gmail.com>
2024-10-25 14:37:36 -0300

    updating price table

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -3,8 +3,8 @@
     <div class="container mx-auto">
       <h2 class="text-3xl font-bold text-center mb-8">Choose Your Plan</h2>
       <LazyScriptStripePricingTable
-        :pricing-table-id="'prctbl_1Q84prP2aEWXlznlThXwZ00r'"
-        :publishable-key="'pk_test_51PxqQFP2aEWXlznl5pMNEs2KFFRxQ4k47rGyUjmmdRJpNYIMqdURdAXlnEOdOhXBL9xoiGKiFbCMEXCKOkedfN5v00FdpFFUiL'"
+        :pricing-table-id="'prctbl_1QDp2D1BO16AwcyZNWPX5eG5'"
+        :publishable-key="'pk_live_51Pv0md1BO16AwcyZZ0veDulS5rW6IVhQsUOmcgswndsyi5mkuzq4sjt3fOKS5jMEroilhMjYtNNUhixL800tqd7Q00AoYDlb1c'"
         v-if="session && session.user"
         :client-reference-id="session.user.id.replace(/\|/g, '_')"
         :customer-email="session.user.email"
